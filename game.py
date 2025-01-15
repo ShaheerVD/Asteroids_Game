0e1de558c5598c4f18e518f840f7b9e487874c9c
Shaheer <64867718+ShaheerVD@users.noreply.github.com>
2025-01-15 14:28:21 +0200

    add models and sprites

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pygame
 from utils import load_sprite
 
@@ -27,3 +28,51 @@ class SpaceRocks:
   def _draw(self):
     self.screen.blit(self.background, (0, 0))
     pygame.display.flip()           
+=======
+import pygame
+from models import GameObject
+from utils import load_sprite
+
+class SpaceRocks:
+  def __init__(self):
+    #initialize the game
+    pygame.init()
+    pygame.display.set_caption('Space Rocks')
+
+    self.screen = pygame.display.set_mode((800, 600))
+    self.background = load_sprite("space", False)
+
+    sprite = load_sprite("spaceship")
+    self.ship = GameObject((400, 300), sprite, (0, 0))
+
+    sprite = load_sprite("asteroid")
+    self.rock = GameObject((50,300), sprite,(0,0))
+
+    self.collision_count = 0
+
+  def main_loop(self):
+    while True:
+      self._handle_input()
+      self._game_logic()
+      self._draw()
+
+  def _handle_input(self):
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        quit()
+
+  def _game_logic(self):
+    self.ship.move()
+    self.rock.move()
+
+  def _draw(self):
+    self.screen.blit(self.background, (0, 0))
+    self.ship.draw(self.screen)
+    self.rock.draw(self.screen)
+    pygame.display.flip()  
+
+    if self.ship.collides_width(self.rock):
+      self.collision_count += 1
+      print(f"Collision #{self.collision_count}")
+             
+>>>>>>> a54774c (add models and sprites)
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from pygame.image import load
 from pathlib import Path
 
@@ -8,4 +9,16 @@ def load_sprite(name,with_alpha=True):
     if with_alpha:
         return sprite.convert_alpha()
     
+=======
+from pygame.image import load
+from pathlib import Path
+
+def load_sprite(name,with_alpha=True):
+    filename = Path(__file__).parent / Path("assets/sprites/"+name+ ".png")
+    sprite = load(filename.resolve())
+
+    if with_alpha:
+        return sprite.convert_alpha()
+    
+>>>>>>> a54774c (add models and sprites)
     return sprite.convert()
