04c1eec1258a26d7e564f39fc517307034b2b04c
Shaheer <64867718+ShaheerVD@users.noreply.github.com>
2025-01-15 15:23:39 +0200

    Add ship movement

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-from models import GameObject
+from models import Spaceship
 from utils import load_sprite
 
 class SpaceRocks:
@@ -7,15 +7,16 @@ class SpaceRocks:
     #initialize the game
     pygame.init()
     pygame.display.set_caption('Space Rocks')
-
+    self.clock = pygame.time.Clock()
+    
     self.screen = pygame.display.set_mode((800, 600))
     self.background = load_sprite("space", False)
 
-    sprite = load_sprite("spaceship")
-    self.ship = GameObject((400, 300), sprite, (0, 0))
+    
+    self.ship = Spaceship((400,300)) 
 
-    sprite = load_sprite("asteroid")
-    self.rock = GameObject((50,300), sprite,(0,0))
+    #sprite = load_sprite("asteroid")
+    #self.rock = GameObject((50,300), sprite,(0,0))
 
     self.collision_count = 0
 
@@ -30,17 +31,27 @@ class SpaceRocks:
       if event.type == pygame.QUIT:
         quit()
 
+    is_key_pressed = pygame.key.get_pressed()
+    if is_key_pressed[pygame.K_ESCAPE] or is_key_pressed[pygame.K_q]:
+      quit()
+    elif is_key_pressed[pygame.K_RIGHT]:
+      self.ship.rotate(clockwise=True)
+    elif is_key_pressed[pygame.K_LEFT]:
+      self.ship.rotate(clockwise=False) 
+    elif is_key_pressed[pygame.K_UP]:
+      self.ship.accelerate()     
+
   def _game_logic(self):
-    self.ship.move()
-    self.rock.move()
+    self.ship.move(self.screen)
+    #self.rock.move()
 
   def _draw(self):
     self.screen.blit(self.background, (0, 0))
     self.ship.draw(self.screen)
-    self.rock.draw(self.screen)
+    #self.rock.draw(self.screen)
     pygame.display.flip()  
-
-    if self.ship.collides_width(self.rock):
-      self.collision_count += 1
-      print(f"Collision #{self.collision_count}")
+    self.clock.tick(30)
+   ## if self.ship.collides_width(self.rock):
+     ## self.collision_count += 1
+      ##print(f"Collision #{self.collision_count}")
              
